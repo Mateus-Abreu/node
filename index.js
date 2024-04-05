a7bb173b1174824dc2f0cabae1cd7d47e0f31b86
Mateus5555577777 <mateusabreu@souunilavras.com>
2024-04-05 08:29:04 -0300

    Alterou2

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ const express = require('express');
 const app = express();
 // Crie um endpoint para receber requisições
 app.get('/', function(req, res) {
- res.send('Ola Mundo!');
+  res.send('Ola Mundo!');
 });
 // Imprima URL para acessar o servidor
 app.listen(8000, function() {
- console.log('App ouvindo na porta 8000!');
+  console.log('App ouvindo na porta 8000!');
 });
+
