f63c53f4d11d3f87845018d3628d71d518bdcb97
Mateus5555577777 <mateusabreu@souunilavras.com>
2024-04-05 19:21:49 -0300

    adicionou scripts

---
+++ model/insert_scripts_cliente.sql
@@ -0,0 +1,8 @@
+INSERT INTO `clientes`(`nome`, `sobrenome`, `email`, `idade`) VALUES
+('Mateus','Abreu','mateusabreu11@email.com','25');
+INSERT INTO `clientes`(`nome`, `sobrenome`, `email`, `idade`) VALUES
+('Vitória','Reis','vitoria@email.com','27');
+INSERT INTO `clientes`(`nome`, `sobrenome`, `email`, `idade`) VALUES
+('José','Silva','jose@email.com','25');
+INSERT INTO `clientes`(`nome`, `sobrenome`, `email`, `idade`) VALUES
+('João','Santos','joao@email.com','19');
---
+++ model/insert_scripts_produto.sql
@@ -0,0 +1,8 @@
+INSERT INTO `produtos`(`nome`, `descricao`, `preco`, `data_atualizado`) VALUES
+('Tenis','Couro','10','05');
+INSERT INTO `produtos`(`nome`, `descricao`, `preco`, `data_atualizado`) VALUES
+('Camiseta','Algodao','5','05');
+INSERT INTO `produtos`(`nome`, `descricao`, `preco`, `data_atualizado`) VALUES
+('Short','Jeans','6','5');
+INSERT INTO `produtos`(`nome`, `descricao`, `preco`, `data_atualizado`) VALUES
+('Meia','Algodao','2','10');
