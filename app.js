f120a76d5cb1163cc4d357d5075db1a279a823fe
Mateus5555577777 <mateusabreu@souunilavras.com>
2024-04-05 14:23:08 -0300

    rotas ok

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var produtosRouter = require('./routes/produtos');
+var clientesRouter = require('./routes/clientes');
 
 var app = express();
 
@@ -21,6 +23,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/produtos', produtosRouter);
+app.use('/clientes', clientesRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
