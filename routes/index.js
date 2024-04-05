118c6f02b5f6d4b42f4272a22e3f83038bb53f93
Mateus5555577777 <mateusabreu@souunilavras.com>
2024-04-05 19:42:27 -0300

    clientes ok

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
+
+
+const mysql = require('mysql2/promise');
+/* GET home page. */
+
+router.get('/', function (req, res, next) {
+ mysql.createConnection({host: 'localhost',user: 'mateus',password: 'mateus',
+ database: 'loja',port: 3306
+ }).then((connection) => {connection.query('SELECT * FROM clientes')
+ .then((result) => {res.send(result[0]);});
+ });
+});
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
